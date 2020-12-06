d310e4c38ba53f12b29c833afd6675cb4da76020
Brandon Grayson <brandongrayson@Brandons-MacBook-Air.local>
2020-12-06 15:26:39 -0500

    set up connection for the 9am button clickEvent

---
+++ script.js
@@ -0,0 +1,11 @@
+// Event Listeners for save buttons
+$('#saveBtn9am').on('click', saveTextArea)
+
+function saveTextArea () {
+    // log button clicked
+    console.log('button was clicked')
+    // access the text area value
+
+    // save it to local storage
+}
+
