36d9f91781f7daacd1c07ccc3b2019f3e8d87b50
Brandon Grayson <brandongrayson@Brandons-MacBook-Air.local>
2020-12-06 22:29:27 -0500

    saveTextBtn class added to all buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // select the text area 
 let nineamTextArea = document.querySelector('#nineamTextArea')
+let saveTextBtn = document.querySelector('.saveTextBtn')
 // $('#nineamTextArea')
 
 // access the data you want from localstorage
@@ -7,25 +8,22 @@ let userText = JSON.parse(localStorage.getItem('userText'))
 
 // 
 if (userText) {
-    console.log(userText)
+    console.log('a value was received')
 } else {
     console.log('we did not get a value')
 }
 
 // access the text area value and save it to a variable
-nineamTextArea.textContent = userText
+nineamTextArea.value = userText
 
 // Event Listeners for save buttons
-saveBtn9am.addEventListener('click', saveTextArea)
+saveTextBtn.addEventListener('click', saveTextArea)
 // J query add event listener version
 //$('#saveBtn9am').on('click', saveTextArea)
 
 function saveTextArea () {
     // log button clicked
     console.log('button was clicked')
-    // save it to local storage
-    // access the text area value and save it to a variable
-    nineamTextArea.textContent = userText
-    localStorage.setItem('userText', userText)
+
 }
 
