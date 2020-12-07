58335b05439773448c599493a5b3383ebee76637
Brandon Grayson <brandongrayson@Brandons-MacBook-Air.local>
2020-12-06 23:35:45 -0500

    added event listener for all buttons

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 // select the text area 
 let nineamTextArea = document.querySelector('#nineamTextArea')
-let saveTextBtn = document.querySelectorAll('.saveTextBtn')
+let saveTextBtns = document.querySelectorAll('.saveTextBtn')
 // $('#nineamTextArea')
 
-// access the data you want from localstorage
-let userText = JSON.parse(localStorage.getItem('userText'))
-
-// 
-if (userText) {
-    console.log('a value was received')
-} else {
-    console.log('we did not get a value')
+// set an event listener for all the buttons
+for (i = 0; i < saveTextBtns.length; i++) {
+    saveTextBtns[i].addEventListener("click", function() {
+        console.log("you clicked"); 
+    });
 }
 
 // access the text area value and save it to a variable
-nineamTextArea.value = userText
+// nineamTextArea.value = userText
 
 // Event Listeners for save buttons
-saveTextBtn.addEventListener('click', saveTextArea)
+// saveTextBtn.addEventListener('click', saveTextArea)
 // J query add event listener version
 //$('#saveBtn9am').on('click', saveTextArea)
-function saveTextArea (event) {
-    // log button clicked
-    let bns = document.getElementsByTagName("button");
-    for (i = 0; i < bns.length; i++) {
-        bns[i].addEventListener("click", function() {
-        console.log("you clicked"); });
-    }
+// function saveTextArea (event) {
+//     // log button clicked
+//     let bns = document.getElementsByTagName("button");
+    
+// }
 
-}
 
+// access the data you want from localstorage
+// let userText = JSON.parse(localStorage.getItem('userText'))
+
+// 
+// if (userText) {
+//     console.log('a value was received')
+// } else {
+//     console.log('we did not get a value')
+// }
